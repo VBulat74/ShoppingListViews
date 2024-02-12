5e0f442be988fa423c68713bd4c94435455ae630
VB <Vladimir.Bulat@gmail.com>
2024-02-12 10:52:31 +0300

    Lesson_14 (layout for ShopItemActivity)

---
+++ app/src/main/java/ru/com/bulat/shoppinglistviews/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package ru.com.bulat.shoppinglistviews.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.com.bulat.shoppinglistviews.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
