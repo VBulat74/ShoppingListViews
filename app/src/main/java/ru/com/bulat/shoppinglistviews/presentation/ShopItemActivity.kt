3637d71d633857281c061944796a9ac3c5300a7e
VB <Vladimir.Bulat@gmail.com>
2024-02-13 14:30:53 +0300

    Lesson_22 (add fragment to main activity)

--- app/src/main/java/ru/com/bulat/shoppinglistviews/presentation/ShopItemActivity.kt
+++ app/src/main/java/ru/com/bulat/shoppinglistviews/presentation/ShopItemActivity.kt
@@ -9,14 +9,6 @@ import ru.com.bulat.shoppinglistviews.domain.ShopItem
 
 class ShopItemActivity : AppCompatActivity() {
 
-//    private lateinit var viewModel: ShopItemViewModel
-//
-//    private lateinit var tilName : TextInputLayout
-//    private lateinit var tilCount : TextInputLayout
-//    private lateinit var etName : EditText
-//    private lateinit var etCount : EditText
-//    private lateinit var buttonSave : Button
-
     private var screenMode : String = MODE_UNKNOWN
     private var shopItemId = ShopItem.UNDEFINED_ID
 
@@ -26,38 +18,10 @@ class ShopItemActivity : AppCompatActivity() {
 
         parseIntent()
 //
-//        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
-//        initViews()
-//        addChangeTextListeners()
-//
-        launchRightMode()
-//
-//        observeViewModel()
+        if (savedInstanceState ==null) {
+            launchRightMode()
+        }
     }
-
-//    private fun observeViewModel() {
-//        viewModel.errorInputCount.observe(this) {
-//            val message = if (it) {
-//                getString(R.string.error_input_count)
-//            } else {
-//                null
-//            }
-//            tilCount.error = message
-//        }
-//
-//        viewModel.errorInputName.observe(this) {
-//            val message = if (it) {
-//                getString(R.string.error_input_name)
-//            } else {
-//                null
-//            }
-//            tilName.error = message
-//        }
-//
-//        viewModel.shouldCloseScreen.observe(this) {
-//            finish()
-//        }
-//    }
 
     private fun launchRightMode () {
         val fragment = when (screenMode) {
@@ -69,47 +33,9 @@ class ShopItemActivity : AppCompatActivity() {
         }
         supportFragmentManager
             .beginTransaction()
-            .add(R.id.shop_item_container,fragment)
+            .replace(R.id.shop_item_container,fragment)
             .commit()
     }
-//
-//    private fun addChangeTextListeners() {
-//        etName.addTextChangedListener(object : TextWatcher {
-//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-//
-//            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//                viewModel.resetErrorInputName()
-//            }
-//
-//            override fun afterTextChanged(p0: Editable?) {}
-//        })
-//        etCount.addTextChangedListener(object : TextWatcher {
-//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-//
-//            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//                viewModel.resetErrorInputCount()
-//            }
-//
-//            override fun afterTextChanged(p0: Editable?) {}
-//        })
-//    }
-//
-//    private fun launchEditMode (){
-//        viewModel.getSopItem(shopItemId)
-//        viewModel.shopItem.observe(this) {
-//            etName.setText(it.name)
-//            etCount.setText(it.count.toString())
-//        }
-//        buttonSave.setOnClickListener {
-//            viewModel.editShopItem(etName.text?.toString(), etCount.text?.toString())
-//        }
-//    }
-//
-//    private fun launchAddMode(){
-//        buttonSave.setOnClickListener {
-//            viewModel.addShopItem(etName.text?.toString(), etCount.text?.toString())
-//        }
-//    }
 
     private fun parseIntent() {
         if (! intent.hasExtra(EXTRA_SCREEN_MODE)) {
@@ -132,15 +58,6 @@ class ShopItemActivity : AppCompatActivity() {
         }
     }
 
-    //
-//    private fun initViews (){
-//        tilName = findViewById(R.id.til_name)
-//        tilCount = findViewById(R.id.til_count)
-//        etName = findViewById(R.id.et_name)
-//        etCount = findViewById(R.id.et_count)
-//        buttonSave = findViewById(R.id.save_button)
-//    }
-//
     companion object {
 
         private const val EXTRA_SCREEN_MODE = "extra_mode"
