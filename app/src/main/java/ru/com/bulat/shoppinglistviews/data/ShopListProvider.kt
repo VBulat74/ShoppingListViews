5b995ca702796d96c58fa0fcd62c51e6660d58e7
VBulat74 <Vladimir.Bulat@gmail.com>
2024-03-02 20:36:09 +0300

    Lesson 34 (UriMatcher)

--- app/src/main/java/ru/com/bulat/shoppinglistviews/data/ShopListProvider.kt
+++ app/src/main/java/ru/com/bulat/shoppinglistviews/data/ShopListProvider.kt
@@ -2,11 +2,17 @@ package ru.com.bulat.shoppinglistviews.data
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 import android.util.Log
 
 class ShopListProvider : ContentProvider() {
+
+    private val uriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
+        addURI( "ru.com.bulat.shoppinglistviews", "shop_items/#", GET_SHOP_ITEMS_QUERY)
+    }
+
     override fun onCreate(): Boolean {
         return true
     }
@@ -18,7 +24,14 @@ class ShopListProvider : ContentProvider() {
         p3: Array<out String>?,
         p4: String?
     ): Cursor? {
-        Log.d("ShopListProvider", "query $uri")
+        val code = uriMatcher.match(uri)
+
+        when(code){
+            GET_SHOP_ITEMS_QUERY -> {
+
+            }
+        }
+        Log.d("ShopListProvider", "query $uri ($code)")
         return null
     }
 
@@ -36,5 +49,9 @@ class ShopListProvider : ContentProvider() {
 
     override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
         TODO("Not yet implemented")
+    }
+
+    companion object{
+        private const val GET_SHOP_ITEMS_QUERY = 100
     }
 }
